4560659f1047f626ae0071046f4b59dadee55aba
ToNi3141 <>
2023-07-16 23:38:47 +0200

    Add MemoryReadRequestGenerator

---
+++ rtl/Rasterix/MemoryReadRequestGenerator.v
@@ -0,0 +1,179 @@
+// Rasterix
+// https://github.com/ToNi3141/Rasterix
+// Copyright (c) 2023 ToNi3141
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+module MemoryReadRequestGenerator #(
+    // Width of the axi interfaces
+    parameter STREAM_WIDTH = 32,
+    // Width of address bus in bits
+    parameter ADDR_WIDTH = 32,
+    // Width of ID signal
+    parameter ID_WIDTH = 8,
+
+    // Sets the width of a pixel to fix the memory alignment
+    parameter PIXEL_WIDTH = 16
+) (
+    input  wire                             aclk,
+    input  wire                             resetn,
+
+    /////////////////////////
+    // Configs
+    /////////////////////////
+    input  wire [ADDR_WIDTH - 1 : 0]        confAddr,
+
+    /////////////////////////
+    // Fragment Interface
+    /////////////////////////
+
+    // Fetch interface
+    input  wire                             s_fetch_axis_tvalid,
+    input  wire                             s_fetch_axis_tlast,
+    output reg                              s_fetch_axis_tready,
+    input  wire [ADDR_WIDTH - 1 : 0]        s_fetch_axis_tdest,
+
+    /////////////////////////
+    // Memory Interface
+    /////////////////////////
+
+    // Address channel
+    output reg  [ID_WIDTH - 1 : 0]          m_mem_axi_arid,
+    output reg  [ADDR_WIDTH - 1 : 0]        m_mem_axi_araddr,
+    output reg  [ 7 : 0]                    m_mem_axi_arlen,
+    output reg  [ 2 : 0]                    m_mem_axi_arsize,
+    output reg  [ 1 : 0]                    m_mem_axi_arburst,
+    output reg                              m_mem_axi_arlock,
+    output reg  [ 3 : 0]                    m_mem_axi_arcache,
+    output reg  [ 2 : 0]                    m_mem_axi_arprot,
+    output reg                              m_mem_axi_arvalid,
+    input  wire                             m_mem_axi_arready
+);
+    localparam ADDR_TAG_POS = $clog2((STREAM_WIDTH) / PIXEL_WIDTH);
+    localparam ADDR_TAG_WIDTH = ADDR_WIDTH - ADDR_TAG_POS;
+
+    reg [ADDR_TAG_WIDTH - 1 : 0] lastAddrTag;
+    reg [ADDR_TAG_WIDTH - 1 : 0] lastAddrTagSkid;
+    reg lastFetchSkid;
+    reg lastFetch;
+    reg memRequest;
+    reg fetchEnabled;
+    reg [ADDR_TAG_WIDTH - 1 : 0] memRequestAddr;
+
+    always @(posedge aclk)
+    begin
+        if (!resetn)
+        begin
+            lastAddrTag <= 0;
+            lastAddrTagSkid <= 0;
+            lastFetchSkid <= 0;
+            lastFetch <= 0;
+            memRequest <= 0;
+
+            m_mem_axi_arid <= 0;
+            m_mem_axi_arlen <= 0; // Use always one beat. If the performance too slow, then we could increase the STREAM_WIDTH and use an external bus converter
+            m_mem_axi_arsize <= { $clog2(STREAM_WIDTH / 8) }[0 +: 3];
+            m_mem_axi_arburst <= 1;
+            m_mem_axi_arlock <= 0;
+            m_mem_axi_arcache <= 0;
+            m_mem_axi_arprot <= 0;
+            m_mem_axi_arvalid <= 0;
+
+            s_fetch_axis_tready <= 1;
+            fetchEnabled <= 0;
+        end
+        else
+        begin
+            // Memory request handling
+            if (!m_mem_axi_arvalid)
+            begin
+                // If no pending memory request is available, then check if a new request has to be issued
+                if (memRequest)
+                begin
+                    m_mem_axi_arvalid <= 1;
+                    m_mem_axi_araddr <= confAddr + { memRequestAddr, { (ADDR_TAG_POS){1'b0} } };
+                    memRequest <= 0;
+                end
+            end
+            else
+            begin
+                // Reset pending request when the arready signal was asserted
+                if (m_mem_axi_arready)
+                begin
+                    m_mem_axi_arvalid <= 0;
+                end
+            end
+
+            // Fetch handling
+            if (s_fetch_axis_tready)
+            begin
+                if (s_fetch_axis_tvalid)
+                begin : Fetch
+                    reg newMemRequest = (((lastAddrTag != s_fetch_axis_tdest[ADDR_TAG_POS +: ADDR_TAG_WIDTH])) && fetchEnabled);
+
+                    // This signal signalizes if at least one address was send via the fetch interface and has initialized lastAddrTag.
+                    // Otherwise a memory request for an previous request with and old address in lastAddrTag could be triggered.
+                    fetchEnabled <= 1;
+                    // Safe current address in the skid buffer when the memory request handling is busy
+                    if (newMemRequest && memRequest)
+                    begin
+                        s_fetch_axis_tready <= 0;
+                        lastAddrTagSkid <= s_fetch_axis_tdest[ADDR_TAG_POS +: ADDR_TAG_WIDTH];
+                        lastFetchSkid <= s_fetch_axis_tlast;
+                    end
+                    else
+                    begin
+                        // If the boundaries of the tag are exceeded, trigger a new write request
+                        if (newMemRequest)
+                        begin
+                            memRequestAddr <= lastAddrTag;
+                            memRequest <= 1;
+                        end
+                        lastAddrTag <= s_fetch_axis_tdest[ADDR_TAG_POS +: ADDR_TAG_WIDTH];
+                        // The last signal is a special case and is handled in the other state
+                        if (s_fetch_axis_tlast)
+                        begin
+                            lastFetch <= 1;
+                            s_fetch_axis_tready <= 0;
+                        end
+                    end
+                end
+            end
+            else
+            begin
+                if (!memRequest)
+                begin
+                    memRequestAddr <= lastAddrTag;
+                    memRequest <= 1;
+                    
+                    if (lastFetch)
+                    begin
+                        // If the current request was the last request in the stream, then reinitialize everything
+                        fetchEnabled <= 0;
+                        lastFetchSkid <= 0;
+                        lastFetch <= 0;
+                        s_fetch_axis_tready <= 1;
+                    end
+                    else
+                    begin
+                        // If the memory request handling is not busy anymore, load the data from the skid buffer.
+                        lastAddrTag <= lastAddrTagSkid;
+                        s_fetch_axis_tready <= !lastFetchSkid;
+                        lastFetch <= lastFetchSkid;
+                    end
+                end
+            end
+        end
+    end
+endmodule
---
+++ unittest/cpp/sim_MemoryReadRequestGenerator.cpp
@@ -0,0 +1,393 @@
+// Rasterix
+// https://github.com/ToNi3141/Rasterix
+// Copyright (c) 2023 ToNi3141
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+// #include "../Unittests/3rdParty/catch.hpp"
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "3rdParty/catch.hpp"
+
+// Include common routines
+#include <verilated.h>
+
+// Include model header, generated from Verilating "top.v"
+#include "VMemoryReadRequestGenerator.h"
+
+void clk(VMemoryReadRequestGenerator* t)
+{
+    t->aclk = 0;
+    t->eval();
+    t->aclk = 1;
+    t->eval();
+}
+
+void reset(VMemoryReadRequestGenerator* t)
+{
+    t->resetn = 0;
+    clk(t);
+    t->resetn = 1;
+    clk(t);
+}
+
+TEST_CASE("Check complete memory request sequence (uninterrupted)", "[MemoryReadRequestGenerator]")
+{
+    VMemoryReadRequestGenerator* t = new VMemoryReadRequestGenerator();
+    t->s_fetch_axis_tlast = 0;
+    t->s_fetch_axis_tvalid = 0;
+    t->m_mem_axi_arready = 1;
+    reset(t);
+
+    t->confAddr = 0x1000'0000;
+
+    // Do this twice, to see, if also a second cycle is possible
+    for (uint32_t i = 0; i < 2; i++)
+    {
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 1. fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 0;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 2. fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 1;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 3. fetch (triggers a mem write). In this clock, the access is prepared, but will be executed in the next clock
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 2;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+
+        // Send 4. fetch. Triggers also the memory request
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 3;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arid == 0);
+        REQUIRE(t->m_mem_axi_arlen == 0);
+        REQUIRE(t->m_mem_axi_arsize == 2);
+        REQUIRE(t->m_mem_axi_arburst == 1);
+        REQUIRE(t->m_mem_axi_arlock == 0);
+        REQUIRE(t->m_mem_axi_arcache == 0);
+        REQUIRE(t->m_mem_axi_arprot == 0);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0000);
+
+        // Send 5. fetch. Deasserts the memory request. Also triggers a new one.
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 4;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 6. fetch. Send the same address as in 5. but with asserted tlast. Memory executes request from 3. and 4.
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tlast = true;
+        t->s_fetch_axis_tdest = 4;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == false); // It is shortly deasserted when the last fetch is coming
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0002);
+
+        // Execute no fetch. Memory request is still pending
+        t->s_fetch_axis_tvalid = false;
+        t->s_fetch_axis_tlast = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Execute no fetch. Memory request is arriving 
+        t->s_fetch_axis_tvalid = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0004);
+
+        // Execute more clock cycles, no additional requests should be executed
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+    }
+
+    // Destroy model
+    delete t;
+}
+
+TEST_CASE("Interrupted fetch stream", "[MemoryReadRequestGenerator]")
+{
+    VMemoryReadRequestGenerator* t = new VMemoryReadRequestGenerator();
+    t->s_fetch_axis_tlast = 0;
+    t->s_fetch_axis_tvalid = 0;
+    t->m_mem_axi_arready = 1;
+    reset(t);
+
+    t->confAddr = 0x1000'0000;
+
+    // Do this twice, to see, if also a second cycle is possible
+    for (uint32_t i = 0; i < 2; i++)
+    {
+        // Send 1. fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 0;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 2.1 fetch
+        t->s_fetch_axis_tvalid = false;
+        t->s_fetch_axis_tdest = 1;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 2.2 fetch
+        t->s_fetch_axis_tvalid = false;
+        t->s_fetch_axis_tdest = 1;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 2.3 fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 1;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 3. fetch.
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 2;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 4.1 fetch.
+        t->s_fetch_axis_tvalid = false;
+        t->s_fetch_axis_tlast = true;
+        t->s_fetch_axis_tdest = 3;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0000);
+
+        // Send 4.2 fetch.
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tlast = true;
+        t->s_fetch_axis_tdest = 3;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == false); // It is shortly deasserted when the last fetch is coming
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Execute no fetch. Memory request is still pending
+        t->s_fetch_axis_tvalid = false;
+        t->s_fetch_axis_tlast = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Execute no fetch. Memory request is arriving 
+        t->s_fetch_axis_tvalid = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0002);
+    }
+
+    // Destroy model
+    delete t;
+}
+
+TEST_CASE("Interrupted memory stream with tlast in skid buffer", "[MemoryReadRequestGenerator]")
+{
+    VMemoryReadRequestGenerator* t = new VMemoryReadRequestGenerator();
+    t->s_fetch_axis_tlast = 0;
+    t->s_fetch_axis_tvalid = 0;
+    t->m_mem_axi_arready = 0;
+    reset(t);
+
+    t->confAddr = 0x1000'0000;
+
+    // Do this twice, to see, if also a second cycle is possible
+    for (uint32_t i = 0; i < 2; i++)
+    {
+        // Send 1. fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 0;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 2. fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 1;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Send 3. fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 2;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+
+        // Send 4. fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 3;
+        clk(t);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0000);
+
+        // Send 5. fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 4;
+        t->s_fetch_axis_tlast = true;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == false);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0000);
+
+        // Mem requests
+        t->s_fetch_axis_tvalid = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == false);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0000);
+
+        // Mem requests
+        t->s_fetch_axis_tvalid = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == false);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0000);
+
+        // Mem requests
+        t->s_fetch_axis_tvalid = false;
+        t->m_mem_axi_arready = true;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == false);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Mem requests
+        t->s_fetch_axis_tvalid = false;
+        t->m_mem_axi_arready = true;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == false);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0002);
+
+        // Mem requests
+        t->s_fetch_axis_tvalid = false;
+        t->s_fetch_axis_tlast = false;
+        t->m_mem_axi_arready = true;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Last mem request
+        t->s_fetch_axis_tvalid = false;
+        t->m_mem_axi_arready = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0004);
+
+        // Last mem request
+        t->s_fetch_axis_tvalid = false;
+        t->m_mem_axi_arready = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0004);
+
+        // Last mem request
+        t->s_fetch_axis_tvalid = false;
+        t->m_mem_axi_arready = true;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Last mem request
+        t->s_fetch_axis_tvalid = false;
+        t->m_mem_axi_arready = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+    }
+
+    // Destroy model
+    delete t;
+}
+
+TEST_CASE("tlast after one clock", "[MemoryReadRequestGenerator]")
+{
+    VMemoryReadRequestGenerator* t = new VMemoryReadRequestGenerator();
+    t->s_fetch_axis_tlast = 0;
+    t->s_fetch_axis_tvalid = 0;
+    t->m_mem_axi_arready = 1;
+    reset(t);
+
+    t->confAddr = 0x1000'0000;
+
+    // Do this twice, to see, if also a second cycle is possible
+    for (uint32_t i = 0; i < 2; i++)
+    {
+        // Send 1. fetch
+        t->s_fetch_axis_tvalid = true;
+        t->s_fetch_axis_tdest = 0;
+        t->s_fetch_axis_tlast = true;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == false);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Wait for memory request
+        t->s_fetch_axis_tvalid = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+
+        // Triggers the memory request
+        t->s_fetch_axis_tvalid = false;
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == true);
+        REQUIRE(t->m_mem_axi_araddr == 0x1000'0000);
+
+        // End
+        clk(t);
+        REQUIRE(t->s_fetch_axis_tready == true);
+        REQUIRE(t->m_mem_axi_arvalid == false);
+    }
+
+    // Destroy model
+    delete t;
+}
